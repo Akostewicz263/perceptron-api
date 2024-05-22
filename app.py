fa3da00e17ee1eb8265ac674abd7032fbeeda25f
Akostewicz263 <andzia2632000@wp.pl>
2024-05-22 21:35:59 +0200

    Update Dockerfile.txt, app.py, and requirements.txt

--- app.py
+++ app.py
@@ -1,32 +1,51 @@
-# app.py
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 
 app = Flask(__name__)
 
+# Załaduj model
 model = joblib.load("model.pkl")
+
+@app.route('/')
+def home():
+    return "Hello World!"
+
+@app.route('/your_name')
+def your_name():
+    return "To jest moja strona"
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    name = request.args.get("name", "")
+    if name:
+        return f"Hello {name}"
+    else:
+        return "Hello"
 
 @app.route('/api/v1.0/predict', methods=['GET'])
 def predict():
-
     x1 = request.args.get("x1", 0, type=float)
     x2 = request.args.get("x2", 0, type=float)
-    
 
-    input_data = np.array([[x1, x2]])
-    
+    # Przewiduj klasę
+    prediction = model.predict(np.array([[x1, x2]]))
 
-    prediction = model.predict(input_data)[0]
-    
-    
-    response = {
-        "features": {"x1": x1, "x2": x2},
-        "predicted_class": prediction
+    # Zmień typ int32 na int
+    prediction = int(prediction[0])
+
+    features = {
+        "x1": x1,
+        "x2": x2
     }
-    
-    
+
+    response = {
+        "prediction": prediction,
+        "features": features
+    }
+
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
+
